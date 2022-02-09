2edf0772e1dacb2f1ad2ab9b1b6d7654e64fddcd
fatiharslan4 <98555897+fatiharslan4@users.noreply.github.com>
2022-02-09 22:03:54 +0300

    Update Response.swift

--- Sources/Response.swift
+++ Sources/Response.swift
@@ -8,7 +8,21 @@
 
 import Foundation
 
+
 public struct Response {
+    public init(url: URL?, finalUrl: URL?, canonicalUrl: String?, title: String?, description: String?, images: [String]?, image: String?, icon: String?, video: String?, price: String?) {
+        self.url = url
+        self.finalUrl = finalUrl
+        self.canonicalUrl = canonicalUrl
+        self.title = title
+        self.description = description
+        self.images = images
+        self.image = image
+        self.icon = icon
+        self.video = video
+        self.price = price
+    }
+    
     
     public internal(set) var url: URL?
     public internal(set) var finalUrl: URL?
